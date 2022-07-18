5e4281278d58810131ac903acbed4c070b2515d5
Jean-Yves Avenard <jya@apple.com>
2022-07-18 23:32:23 +1000

    WebM file fail to play https://bugs.webkit.org/show_bug.cgi?id=242854 <rdar://97190939>
    Reviewed by NOBODY (OOPS!).
    
    If a track is disabled because it contains no data, the AV MediaPlayer will
    flag the content at not playbable even though it will play.
    
    We can't properly test this change for the failing file as we rely on the
    MediaFormatReader plugin to decode WebM which isn't available on the EWS bots.
    
    * Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.h:
    * Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm:
    (WebCore::MediaPlayerPrivateAVFoundationObjC::allTracksArePlayable const):
    (WebCore::MediaPlayerPrivateAVFoundationObjC::containsDisabledTracks const):
    (WebCore::MediaPlayerPrivateAVFoundationObjC::assetStatus const):

--- Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm
+++ Source/WebCore/platform/graphics/avfoundation/objc/MediaPlayerPrivateAVFoundationObjC.mm
@@ -1830,10 +1830,28 @@ std::optional<bool> MediaPlayerPrivateAVFoundationObjC::allTracksArePlayable() c
         return std::nullopt;
 
     for (AVAssetTrack *assetTrack : [m_avAsset tracks]) {
-        if (!trackIsPlayable(assetTrack))
+        if ([assetTrack isEnabled] && !trackIsPlayable(assetTrack))
             return false;
     }
     return true;
+}
+
+bool MediaPlayerPrivateAVFoundationObjC::containsDisabledTracks() const
+{
+    if (m_avPlayerItem) {
+        for (AVPlayerItemTrack *track in [m_avPlayerItem tracks]) {
+            if (![track isEnabled])
+                return true;
+        }
+        return false;
+    }
+
+    ASSERT(m_avAsset);
+    for (AVAssetTrack *assetTrack : [m_avAsset tracks]) {
+        if (![assetTrack isEnabled])
+            return true;
+    }
+    return false;
 }
 
 bool MediaPlayerPrivateAVFoundationObjC::trackIsPlayable(AVAssetTrack* track) const
@@ -1922,7 +1940,7 @@ MediaPlayerPrivateAVFoundation::AssetStatus MediaPlayerPrivateAVFoundationObjC::
     }
 
     if (!m_cachedAssetIsPlayable)
-        m_cachedAssetIsPlayable = [[m_avAsset valueForKey:@"playable"] boolValue];
+        m_cachedAssetIsPlayable = [[m_avAsset valueForKey:@"playable"] boolValue] || containsDisabledTracks();
 
     if (*m_cachedAssetIsPlayable && *m_cachedTracksArePlayable)
         return MediaPlayerAVAssetStatusPlayable;
